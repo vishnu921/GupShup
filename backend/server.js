11fafc498e63b70469079b9e02692c9f079bbd8d
vishnu0921 <ba11@iitbbs.ac.in>
2022-10-09 19:38:22 +0530

    Added message api

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db')
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
 const userRoutes = require('./routes/userRoutes')
 const chatRoutes = require('./routes/chatRoutes')
+const messageRoutes = require('./routes/messageRoutes')
 const chats = [
   {
     isGroupChat: false,
@@ -125,6 +126,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
+app.use('/api/message', messageRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
