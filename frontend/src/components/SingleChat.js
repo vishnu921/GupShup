432c1f52c37525c069c40d507caa949b1a7992f3
vishnu0921 <ba11@iitbbs.ac.in>
2022-10-10 22:50:38 +0530

    heroku deploy

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -12,7 +12,7 @@ import io from 'socket.io-client'
 import Lottie from "react-lottie"
 import animationData from "../animations/typing.json"
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://gupshup-mern.herokuapp.com/";
 let socket, SelectedChatCompare;
 
 const SingleChat = ({fetchAgain, setFetchAgain }) => {
