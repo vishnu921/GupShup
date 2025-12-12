5f6e825b9cfc6c1d17d471a330f0512cf5c2369a
Bishnu Amola <ba11@iitbbs.ac.in>
2025-12-12 16:14:37 +0530

    refactor redis client code

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -2,37 +2,38 @@ const Redis = require("ioredis");
 
 const REDIS_URL = process.env.REDIS_URL;
 
-function registerEvents(client) {
+function registerEvents(client, name) {
+    const logPrefix = `REDIS Event :: ${name}`;
     client.on('error', (err) => {
-        console.error('Redis connection error:', err);
+        console.error(`${logPrefix} :: Connection error:`, err);
         process.exit(1);
     });
 
     // Fired when Redis is actually ready for commands
     client.on('ready', () => {
-        console.log('Redis connection established (ready).');
+        console.log(`${logPrefix} :: Connection established (ready).`);
     });
 
     // Fired when the client successfully reconnects
     client.on('reconnecting', () => {
-        console.log('Redis is reconnecting...');
+        console.log(`${logPrefix} :: Redis is reconnecting...`);
     });
 
     // Fired when the connection is dropped
     client.on('end', () => {
-        console.log('Redis connection closed / ended.');
+        console.log(`${logPrefix} :: Connection closed / ended.`);
         process.exit(1);
     });
 }
 
-function getClient(url) {
+function getClient(url, name) {
     const client = new Redis(url);
-    registerEvents(client);
+    registerEvents(client, name);
     return client;
 }
 
-const pubClient = getClient(REDIS_URL);
-const subClient = getClient(REDIS_URL);
+const pubClient = getClient(REDIS_URL, 'PUBLISHER');
+const subClient = getClient(REDIS_URL, 'SUBSCRIBER');
 
 module.exports = {
     pubClient,
