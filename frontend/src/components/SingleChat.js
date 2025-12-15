7686dfd23a4834c1235f7451200944d557286302
Bishnu Amola <ba11@iitbbs.ac.in>
2025-12-15 20:37:59 +0530

    added socket endpoint in env

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -12,7 +12,7 @@ import io from 'socket.io-client'
 import Lottie from "react-lottie"
 import animationData from "../animations/typing.json"
 
-const ENDPOINT = "https://gupshup-mern.herokuapp.com/";
+const ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT;
 let socket, SelectedChatCompare;
 
 const SingleChat = ({fetchAgain, setFetchAgain }) => {
