ce744888af4ef61675b88712d8a5af1281f1cecb
Bishnu Amola <ba11@iitbbs.ac.in>
2025-12-17 21:21:39 +0530

    remove proxy request and add base url in .env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import ChatProvider from './context/ChatProvider';
 
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
